2842e6272b256eeaf4345e99e9fd89f3e0c37077
Ashley Kim <kitetale248@gmail.com>
2022-09-22 03:32:45 -0400

    changed vector indexing type from uint to int for window build

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -90,5 +90,5 @@ struct PlayMode : Mode {
 	uint16_t answerCount = 0;
 	uint16_t pressCount = 0;
 
-	uint16_t index = 0; //indexing through answer vector in update
+	int index = 0; //indexing through answer vector in update
 };
